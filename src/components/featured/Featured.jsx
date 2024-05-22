1b566489143f8ebb20e8e1ccf58f423d50a19531
chiedozieu <chiedozieugwor@gmail.com>
2024-05-22 16:39:50 +0100

    features UI done

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,12 +1,28 @@
  import styles from './featured.module.css'
+ import Image from 'next/image'
+ import beach from '/public/beach.jpeg'
  
  const Featured = () => {
    return (
      <div className={styles.container}>
-       Featured
+       <h className={styles.title}>
+       <b>Shape Your World! </b> Top Stories, Breaking News...
+       </h>
+       <div className={styles.post}>
+          <div className={styles.imgContainer}>
+            <Image src={beach} alt='' fill className={styles.Image} />
+          </div>
+          <div className={styles.textContainer}>
+              <h1 className={styles.postTitle}>Your News, Your Way. Breaking News, Headlines, and More</h1>
+              <p className={styles.postDescription}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid deleniti explicabo ut dicta quae facere impedit mollitia eos, fuga assumenda, a perspiciatis. Fugit voluptatum sequi suscipit quas, explicabo magnam rerum.
+              </p>
+              <button className={styles.button}>Read More</button>
+          </div>
+       </div>
      </div>
    )
  }
  
  export default Featured
- 
+  
